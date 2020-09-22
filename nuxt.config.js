f4f10058b7cd1c2b5672867992385dd14c2585e3
zu-min-g <68736447+zu-min-g@users.noreply.github.com>
2020-09-22 17:59:32 +0900

    viewport を指定

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,12 @@ export default {
     router: {
         base: '/qrcode-demo/',
     },
+    head: {
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        ],
+    },
     components: true,
     buildModules: ['@nuxt/typescript-build'],
     target: 'static',
